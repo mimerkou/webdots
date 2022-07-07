0b2e0cfae12c569fd7d3ce86001ab59c1d5dc7d4
Michalis Merkouris <mimerkou@gmail.com>
2022-07-07 19:53:51 +0300

    finished changing Home styling to styled components, but not home-links keyframes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,36 +3,49 @@ import LogoLarge from '../../assets/logo-large.png';
 import { Link } from 'react-router-dom';
 import { FaRegLightbulb } from 'react-icons/fa';
 import { BsPencilSquare, BsTools, BsGraphUp } from 'react-icons/bs';
+
+import {
+  HomeContainer,
+  Left,
+  LineAndText,
+  Line,
+  Text,
+  WebdotsAndWorks,
+  Webdots,
+  DigitalAgency,
+  Description,
+  HomeLinks,
+  Right,
+  ImageContainer,
+  Image,
+} from './HomeElements';
+
 import './Home.css';
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <div className="left">
-        <div className="line-and-text">
-          <div className="line"></div>
-          <div className="text">
-            <div className="webdots-and-works">
-              <h1 className="webdots">WebDots</h1>
+    <HomeContainer>
+      <Left>
+        <LineAndText>
+          <Line />
+          <Text>
+            <WebdotsAndWorks>
+              <Webdots>WebDots</Webdots>
               <Link to="/works" className="home-works">
                 View our
                 <br />
                 Works
               </Link>
-            </div>
-            <div className="digital-agency">
-              <h2>Digital Agency</h2>
-            </div>
-            <div className="description">
-              <p>
-                We leverage years of experience in online business and help our
-                clients achieve their goals.
-              </p>
-            </div>
-          </div>
-        </div>
+            </WebdotsAndWorks>
+            <DigitalAgency>Digital Agency</DigitalAgency>
+            <Description>
+              We leverage years of experience in online business and help our
+              clients achieve their goals.
+            </Description>
+          </Text>
+        </LineAndText>
 
-        <div className="home-links">
+        <HomeLinks>
           <Link to="/" className="strategy">
             <FaRegLightbulb />
             <p>Strategy</p>
@@ -49,15 +62,15 @@ const Home = () => {
             <BsGraphUp />
             <p>Marketing</p>
           </Link>
-        </div>
-      </div>
+        </HomeLinks>
+      </Left>
 
-      <div className="right">
-        <div className="image">
-          <img src={LogoLarge} alt="Logo" />
-        </div>
-      </div>
-    </div>
+      <Right>
+        <ImageContainer>
+          <Image src={LogoLarge} alt="Logo" />
+        </ImageContainer>
+      </Right>
+    </HomeContainer>
   );
 };
 
