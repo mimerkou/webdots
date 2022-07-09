196e31d2f65ae85e11e02b72de3392d39c4a5b42
Michalis Merkouris <mimerkou@gmail.com>
2022-07-09 13:00:53 +0300

    setup pages for router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Services from './pages/Services/Services';
+import OurWorks from './pages/OurWorks/OurWorks';
+import Contact from './pages/Contact/Contact';
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,6 +23,10 @@ const App = () => {
         <Sidebar isOpen={isOpen} toggle={toggle} />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/our-works" element={<OurWorks />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/OurWorks/OurWorks.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './OurWorks.css';
+
+const OurWorks = () => {
+  return <div>OurWorks</div>;
+};
+
+export default OurWorks;
